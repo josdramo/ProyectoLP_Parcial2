3b74b11f9a39fc37e3ebf94d84d902e405bbfd77
josdramo <josdramo@espol.edu.ec>
2025-01-17 15:46:51 -0500

    ruta eventos arreglada

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,16 @@
 <template>
-  <div>
-    <HeaderComponent />
-    <SliderComponent :slides="slides" />
-    <TrendingCategoryComponent :featuredPost="featuredPost" :posts="posts" />
-    <FooterComponent/>
+  <div id="app">
+    <!-- Renderiza solo el contenido dinámico de las rutas -->
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
-import HeaderComponent from "./components/HeaderComponent.vue";
-import SliderComponent from "./components/SliderComponent.vue";
-import TrendingCategoryComponent from "./components/TrendingCategoryComponent.vue";
-import FooterComponent from "./components/FooterComponent.vue";
-
 export default {
-  components: {
-    HeaderComponent,
-    SliderComponent,
-    TrendingCategoryComponent,
-    FooterComponent
-  },
-  data() {
-    return {
-      slides: [
-        {
-          image: "assets/img/post-slide-1.jpg",
-          title: "The Best Homemade Masks for Face",
-          description: "Lorem ipsum dolor sit amet...",
-          link: "single-post.html",
-        },
-        // Más diapositivas
-      ],
-      featuredPost: {
-        image: "assets/img/post-landscape-1.jpg",
-        title: "11 Work From Home Part-Time Jobs...",
-        description: "Lorem ipsum dolor sit...",
-        date: "Jul 5th '22",
-        category: "Culture",
-      },
-      posts: [
-        [
-          {
-            image: "assets/img/post-landscape-2.jpg",
-            title: "Let’s Get Back to Work, New York",
-            date: "Jul 5th '22",
-            category: "Sport",
-          },
-          // Más posts
-        ],
-        // Más columnas
-      ],
-    };
-  },
+  name: "App",
 };
 </script>
+
+<style>
+/* Estilos globales opcionales */
+</style>
--- src/router.js
+++ src/router.js
@@ -1,16 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './App.vue'; // Tu componente base
-import EventManager from './components/EventManager.vue'; // Componente para organizar eventos
+import { createRouter, createWebHistory } from "vue-router";
+import HomeComponent from "./components/HomeComponent.vue"; // Nueva vista principal
+import EventManager from "./components/EventManager.vue"; // Vista para gestionar eventos
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home, // Ruta principal
+    path: "/",
+    name: "Home",
+    component: HomeComponent, // Ruta principal
   },
   {
-    path: '/events',
-    name: 'EventManager',
+    path: "/events",
+    name: "EventManager",
     component: EventManager, // Ruta para organizar eventos
   },
 ];
