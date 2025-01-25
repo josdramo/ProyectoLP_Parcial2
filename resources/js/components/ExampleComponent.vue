bea9bb465cc18d0fbabedeb773f584de68a4014c
josdramo <josdramo@espol.edu.ec>
2025-01-24 20:09:08 -0500

    revisión de vite.config

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "¡Hola desde Vue.js!",
+        };
+    },
+};
+</script>
+
+<style scoped>
+h1 {
+    color: #42b983;
+}
+</style>
